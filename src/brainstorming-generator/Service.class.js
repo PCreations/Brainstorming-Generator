57b1227970f4d40048eba48b6b80277accff34cf
Pierre Criulanscy <pcriulan@gmail.com>
2012-01-19 18:57:49 +0100

    Migration vers la classe WordManager et modif fonction de selection du nombre de mots a recuperer par sens

--- src/brainstorming-generator/Service.class.js
+++ src/brainstorming-generator/Service.class.js
@@ -5,8 +5,6 @@ var Service = Class.extend({
 	parameters: '',
 	serviceURL: '',
 	BASE_URL : "http://id.asianwordnet.org/services/",
-	WORD_LIMIT: 10,
-	WORD_MAX_PER_SENSE: 3,
 	
 	//constructor
 	init: function(searchWord, output) {
@@ -14,6 +12,7 @@ var Service = Class.extend({
 		this.output = output;
 		this.selectedWords = new Array();
 		this.numberWordBySenses = new Array(); //contiendra la liste de mot a recuperer pour chaque sens dans une limite de WORD_MAX_PER_SENSE mots par sens et WORD_LIMIT mots en tout
+		this.wm = new WordManager();
 	},
 	
 	getServiceURL: function() {
@@ -34,108 +33,16 @@ var Service = Class.extend({
 		});
 	},
 	
-	
-	setNumberWordBySenses: function(numberOfSenses) {
-		console.log("Nombre de sens : "+numberOfSenses);
-		var numberOfWordBySenses = Math.floor(this.WORD_LIMIT / numberOfSenses); //on obtient ainsi le nombre moyen de mots par sens. 
-		console.log("Nombre de mots par sens : "+numberOfWordBySenses);
-		var unity = numberOfWordBySenses - 1 //permet de savoir le nombre d'"unites" a disposition pour choisir un nombre aleatoire (correspond au nombre max du rand d'ou le -1)
-		for (var i=1; i<=numberOfSenses; i++) {
-			console.log("Nombre d'unités : "+unity);
-			var numberOfWord;
-			if (i != numberOfSenses) { // on peut definir aleatoirement le nombre de mot a piocher en fonction des unites
-				numberOfWord = Math.floor(Math.random() * unity)+1; //nombre de mot a piocher pour ce sens
-				console.log("Nombre de mots à piocher : "+numberOfWord+"\n");
-				unity = unity - numberOfWord + numberOfWordBySenses;
-			}
-			else {
-				numberOfWord = unity + 1;
-			}
-			this.numberWordBySenses.push(numberOfWord);
-		}
-		console.log(this.numberWordBySenses);
-		/*ex : 5 sens, nombre de mots par sens = 2
-		Premier tour : 2 unités
-			Ont défini aléatoirement le nombre d'unité alouée au premier sens : 
-			1
-		Deuxième tour : 2-1 + 2 = 3 unités
-			3
-		3ème tour : 3-3 + 2 = 2 unités
-			2
-		4ème tour : 2-2 + 2 = 2 unités
-			2
-		5ème tour : 2-2 + 2 = 2 unités
-			Obligé de prendre 2*/
-	},
-	
 	getInfos: function() {
 		console.log(this);
 	},
 	
 	process: function(json, _this) {
-		var words = _this.parseWords(json, _this);
+		var words = _this.wm.parseWords(json, _this.searchWord);
 		console.log(words);
-		this.setNumberWordBySenses(words.length);
-		_this.selectWords(words, _this);
-		console.log(_this.selectedWords);
-	},
-	
-	parseWords: function(json, _this) {
-		var words = new Array();
-		for (var word in json['data']) {
-			words.push(new Word(
-												json['data'][word]['synset_offset'], 
-												json['data'][word]['words'],
-												json['data'][word]['ss_type'],
-												json['data'][word]['sense_id'],
-												json['data'][word]['gloss']
-											));
-		}
-		words = _this.cleanWords(words, _this);
-		return words;
-	},
-	
-	
-	//Fonction permettant de garder uniquement les mots differents de celui d'origine dans la liste des mots recuperes
-	cleanWords: function(words, _this) {
-		for (var word in words) {
-			var wordList = words[word].word;
-			words[word].word = wordList.replace(_this.searchWord, "");
-			words[word].gloss = words[word].gloss.replace(";", "");
-		}
-		return words;
-	},
-	
-	//Permet de choisir aleatoirement un mot dans un chaine en specifiant le delimiteur des mots
-	pickOneWord: function(delimiter, string) {
-		var splittedString = string.split(delimiter);
-		var numberOfWords = splittedString.length;
-		do {
-			var randomKey = Math.floor(Math.random() * numberOfWords);
-		}while (splittedString[randomKey] == "" || splittedString[randomKey] == " ");
-		return splittedString[randomKey];
-	},
-	
-	//Permet de choisir un mot au hasard dans une chaine en supprimant les mots les plus courants et en ne selectionnant que les noms dans un premier temps
-	pickRelevantWord: function(string, _this) {
-		//Premiere etape : supression des mots les plus courants
-		var ws = new WordSelector(); //objet gerant la selection des mots
-		
-		//on separe la chaine en tableau pour isoler les mots
-		var splittedString = string.split(" ");
-		
-		//on parcours les mots un a un et on supprimme ceux qui sont trop communs
-		for (var i in splittedString) {
-			if (ws.inArray(splittedString[i], ws.commonsWords)) { //si le mot est trouve alors on le supprime
-				splittedString[i] = "";
-			}
-		}
-		
-		//on reconstitue la chaine avec possiblement beaucoup d'espace mais pas derangeant
-		var string = splittedString.join(" ");
-		
-		//on choisit ensuite un mot au hasard parmi la liste
-		return _this.pickOneWord(" ", string);
+		_this.numberWordBySenses = _this.wm.setNumberWordBySenses(words.length);
+		/*_this.wm.selectWords(words, _this.selectedWords);
+		console.log(_this.selectedWords);*/
 	},
 	
 	//Permet d'ajouter un mot dans la liste des mots selectionnes
--- src/brainstorming-generator/WordManager.class.js
+++ src/brainstorming-generator/WordManager.class.js
@@ -1,4 +1,7 @@
-var WordSelector = Class.extend({
+var WordManager = Class.extend({
+	
+	WORD_LIMIT: 50,
+	WORD_MAX_PER_SENSE: 3,
 	
 	init: function(){
 		this.commonsWords = new Array("a", "able", "about", "above", "act", "add", "afraid", "after", "again", "against", "age", "ago", "agree", "all", "almost", "alone", "along", "already", "also", "although", "always", "am", "amount", "an", "and", "anger", "angry", "animal", "another", "answer", "any", "appear", "apple", "are", "arrive", "arm", "arms", "around", "arrive", "as", "ask", "at", "attempt", "aunt", "away", "back", "bad", "bag", "bay", "be", "became", "because", "become", "been", "before", "began", "begin", "behind", "being", "bell", "belong", "below", "beside", "best", "better", "between", "beyond", "big", "body", "bone", "born", "borrow", "both", "bottom", "box", "boy", "break", "bring", "brought", "bug", "built", "busy", "but", "buy", "by", "call", "came", "can", "cause", "choose", "close", "close", "consider", "come", "consider", "considerable", "contain", "continue", "could", "cry", "cut", "dare", "dark", "deal", "dear", "decide", "deep", "did", "die", "do", "does", "dog", "done", "doubt", "down", "during", "each", "ear", "early", "eat", "effort", "either", "else", "end", "enjoy", "enough", "enter", "even", "ever", "every", "except", "expect", "explain", "fail", "fall", "far", "fat", "favor", "fear", "feel", "feet", "fell", "felt", "few", "fill", "find", "fit", "fly", "follow", "for", "forever", "forget", "from", "front", "gave", "get", "gives", "goes", "gone", "good", "got", "gray", "great", "green", "grew", "grow", "guess", "had", "half", "hang", "happen", "has", "hat", "have", "having", "he", "hear", "heard", "held", "hello", "help", "her", "here", "hers", "high", "hill", "him", "his", "hit", "hold", "hot", "how", "however", "I", "if", "ill", "in", "indeed", "instead", "into", "iron", "is", "it", "its", "just", "keep", "kept", "knew", "know", "known", "late", "least", "led", "left", "lend", "less", "let", "like", "likely", "likr", "lone", "long", "look", "lot", "make", "many", "may", "me", "mean", "met", "might", "mile", "mine", "moon", "more", "most", "move", "much", "must", "my", "near", "nearly", "necessary", "neither", "never", "next", "no", "none", "nor", "not", "note", "nothing", "now", "number", "of", "off", "often", "oh", "on", "once", "only", "or", "other", "ought", "our", "out", "please", "prepare", "probable", "pull", "pure", "push", "put", "raise", "ran", "rather", "reach", "realize", "reply", "require", "rest", "run", "said", "same", "sat", "saw", "say", "see", "seem", "seen", "self", "sell", "sent", "separate", "set", "shall", "she", "should", "side", "sign", "since", "so", "sold", "some", "soon", "sorry", "stay", "step", "stick", "still", "stood", "such", "sudden", "suppose", "take", "taken", "talk", "tall", "tell", "ten", "than", "thank", "that", "the", "their", "them", "then", "there", "therefore", "these", "they", "this", "those", "though", "through", "till", "to", "today", "told", "tomorrow", "too", "took", "tore", "tought", "toward", "tried", "tries", "trust", "try", "turn", "two", "under", "until", "up", "upon", "us", "use", "usual", "various", "verb", "very", "visit", "want", "was", "we", "well", "went", "were", "what", "when", "where", "whether", "which", "while", "white", "who", "whom", "whose", "why", "will", "with", "within", "without", "would", "yes", "yet", "you", "young", "your", "br", "img", "p","lt", "gt", "quot", "copy");
@@ -20,6 +23,92 @@ var WordSelector = Class.extend({
 			} while (i--);
 		}
 		return false;
-	}
+	},
+	
+	parseWords: function(json, searchWord) {
+		var words = new Array();
+		console.log(searchWord);
+		this.searchWord = searchWord;
+		for (var word in json['data']) {
+			words.push(new Word(
+												json['data'][word]['synset_offset'], 
+												json['data'][word]['words'],
+												json['data'][word]['ss_type'],
+												json['data'][word]['sense_id'],
+												json['data'][word]['gloss']
+											));
+		}
+		words = this.cleanWords(words);
+		return words;
+	},	
+	
+	strReplace: function(delimiter, needle, string, replaceBy) {
+		//on separe la chaine en tableau pour isoler les mots
+		var splittedString = string.split(delimiter);
+		for (var i in splittedString) {
+			if (splittedString[i].search(needle) != -1)
+				splittedString[i] = replaceBy;
+		}
+		
+		return splittedString.join(",");
+	},
+	
+	//Fonction permettant de garder uniquement les mots differents de celui d'origine dans la liste des mots recuperes
+	cleanWords: function(words) {
+		console.log("Mot recherché : "+this.searchWord);
+		for (var word in words) {
+			var wordList = words[word].word;
+			var regexp = "/"+this.searchWord+"/g";
+			words[word].word = this.strReplace(',', this.searchWord, wordList, '');
+			words[word].gloss = words[word].gloss.replace(";", "");
+		}
+		return words;
+	},
+	
+	//Permet de choisir aleatoirement un mot dans un chaine en specifiant le delimiteur des mots
+	pickOneWord: function(delimiter, string) {
+		var splittedString = string.split(delimiter);
+		var numberOfWords = splittedString.length;
+		do {
+			var randomKey = Math.floor(Math.random() * numberOfWords);
+		}while (splittedString[randomKey] == "" || splittedString[randomKey] == " ");
+		return splittedString[randomKey];
+	},
+	
+	//Permet de choisir un mot au hasard dans une chaine en supprimant les mots les plus courants et en ne selectionnant que les noms dans un premier temps
+	pickRelevantWord: function(string, _this) {
+		//Premiere etape : supression des mots les plus courants
+		var ws = new WordSelector(); //objet gerant la selection des mots
+		
+		
+		
+		//on choisit ensuite un mot au hasard parmi la liste
+		return _this.pickOneWord(" ", string);
+	},
+	
+	setNumberWordBySenses: function(numberOfSenses) {
+		var numberWordBySenses = new Array();
+		console.log("Nombre de sens : "+numberOfSenses);
+		var numberOfWordBySenses = Math.floor(this.WORD_LIMIT / numberOfSenses); //on obtient ainsi le nombre moyen de mots par sens. 
+		console.log("Nombre de mots par sens : "+numberOfWordBySenses);
+		var unity = numberOfWordBySenses - 1 //permet de savoir le nombre d'"unites" a disposition pour choisir un nombre aleatoire (correspond au nombre max du rand d'ou le -1)
+		for (var i=1; i<=numberOfSenses; i++) {
+			console.log("Nombre d'unités : "+unity);
+			var numberOfWord;
+			if (i != numberOfSenses) { // on peut definir aleatoirement le nombre de mot a piocher en fonction des unites
+				do {
+					numberOfWord = Math.floor(Math.random() * unity)+1; //nombre de mot a piocher pour ce sens
+				}while (numberOfWord > this.WORD_MAX_PER_SENSE);
+				console.log("Nombre de mots à piocher : "+numberOfWord+"\n");
+				unity = unity - numberOfWord + numberOfWordBySenses;
+			}
+			else {
+				numberOfWord = (++unity > this.WORD_MAX_PER_SENSE) ? this.WORD_MAX_PER_SENSE : unity;
+			}
+			numberWordBySenses.push(numberOfWord);
+		}
+		console.log(numberWordBySenses);
+		return numberWordBySenses;
+	},
 	
 });
